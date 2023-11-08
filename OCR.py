860d78688d5112f04cc8fbe4c25ef0d9fe00d9fb
Coder-Pradeep-999 <94434721+Coder-Pradeep-999@users.noreply.github.com>
2023-11-08 20:33:35 +0530

    Create OCR.py

---
+++ OCR.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pytesseract
+import cv2
+
+# Setting environment variable for pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+# Opening an image with cv2
+filename = 'path to an image from which text has to be extacted'
+img = cv2.imread(filename)
+
+# Processing the image to denoise, deblur and sharpen the image
+norm_img = np.zeros((img.shape[0], img.shape[1]))
+img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
+img = cv2.threshold(img, 100, 255, cv2.THRESH_TOZERO)[1]
+img = cv2.GaussianBlur(img, (1, 1), 0)
+
+# Displaying the processed image
+cv2.imshow("Processed Image",img)
+cv2.waitKey(0)
+
+# Generating text from the image using OCR
+text = pytesseract.image_to_string(img)
+
+print(text)
